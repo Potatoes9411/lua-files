eb1675a8563dcd66d3125634942bcc1cb7a43541
Potatoes9411 <70236001+Potatoes9411@users.noreply.github.com>
2026-05-20 15:53:04 -0400

    Upload 3159330.lua

---
+++ 3159330.lua
@@ -0,0 +1,54 @@
+-- Made by Potatoes9411 (UserID:892887847873445958), If you would like to join my server if you aren't already in it, here is a link (https://discord.gg/myHf6qmAdj)
+-- If you're interested in distributing/sharing these files please dm me on discord (UserID:892887847873445958)
+-- And if you have any questions feel free to dm on discord (UserID:892887847873445958)
+-- If you are wondering what other games I own, check out my steam here: https://steamcommunity.com/id/Potatoes9411/
+
+
+-- Socials: https://guns.lol/potatoes9411
+
+
+-- Also If you want your game to auto update, make the "setManifestid" lines all have "--" infront of them, so they would look like "--setManifestid". 
+-- Or you can use a tool a good friend of mine made, heres the link: https://github.com/madoiscool/LuaTools/releases, click the "LuaTools.exe" to download it, then run it, then click patch luas and let it change all your luas to have the "--" stuff, then restart steam and all your games will auto update (P.S. If you add a new game you will need to click patch luas again, or you can drag the zip of the mani and lua files into the box in luatools and will add it and make it auto update.)
+
+
+-- Main stuff to add your game.
+-- ------------------------------------------------------------
+-- 3159330's Lua and Manifest Created by Potatoes9411
+-- unknown
+-- Created: May 17, 2026 at 06:59:18 EDT
+-- Website: https://potatoes-dev.com
+-- Total Depots: 0
+-- Total DLCs: 8
+-- Blacklisted Depots: 0
+
+addappid(3159330)
+addappid(3159340)
+addappid(3362520)
+addappid(3362530)
+addappid(3362540)
+addappid(3362550)
+addappid(3362560)
+addappid(3362570)
+addappid(3362580)
+addappid(3362610)
+addappid(3362620)
+addappid(3362630)
+addappid(3362640)
+addappid(3931580)
+addappid(3931590)
+addappid(1716751,0,"84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675")
+--setManifestid(1716751,"4132506267642462151")
+addappid(3159331,0,"a1a2865a35a1aa7255e0d1a8d6cedab23f2b34346e4a359a283f1a5ceb826ddf")
+--setManifestid(3159331,"6820812093185582032")
+addappid(3159333,0,"24bbf4e14d616c7df7c56696e444569bcf5689554a6a79f731426e4fac7d66ea")
+--setManifestid(3159333,"93464155184636835")
+addappid(3159334,0,"919bdf5d6752a3dfc2cf5f76c6800e25059aeab0489c2ec656e1bb938fef0f13")
+--setManifestid(3159334,"8674127046926576802")
+addappid(3159335,0,"843ebde3282fd10fe2204126d7be3c471c849e01618fd2419fd56be6a2e4fc72")
+--setManifestid(3159335,"5545237364594950624")
+addappid(3159336,0,"10867627410a4ed3756f1263fe75741717e2c6f7797ff9af89cd7c5749a14f2b")
+--setManifestid(3159336,"3127422914068385905")
+addappid(3159337,0,"e4e59c05f264ffc35d4c0629f2f887bee3dc7848b240411d65a8f2e2d793843b")
+--setManifestid(3159337,"5339982399734190874")
+addappid(3159339,0,"bf213689d9d9dbd0ec6f827e3af29ed5783bb4da152bbefe251a4af6197755f5")
+--setManifestid(3159339,"6582021815043815936")
