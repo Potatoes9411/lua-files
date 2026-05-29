8182a43f5adb347f5048964091ede191b26bb2fa
Potatoes9411 <70236001+Potatoes9411@users.noreply.github.com>
2026-05-29 16:07:44 -0400

    Add files via upload

---
+++ 3768760.lua
@@ -0,0 +1,36 @@
+-- Made by F1uxin, Discord ID:965053505901588521 | Discord Server (TGP | The Galaxy of Pirates): https://discord.gg/VfCxRsFyFe
+
+-- If you're interested in distributing these files please dm me on discord (UserID:965053505901588521) 
+-- (I don't care if you send them to a friend or to help someone, just if you add them in your bot or have them widely available then i would like to know.)
+
+-- Socials: https://guns.lol/f1uxin
+
+-- Also If you want your game to auto update, make the "setManifestid" lines all have "--" infront of them, so they would look like "--setManifestid", save the file, then add the lua file, (i think you can add the manifests too but I would just add the lua file to ensure it works).
+
+-- If you have any questions feel free to dm on discord (UserID:965053505901588521)
+
+
+-- Main stuff to actually add the game, DLCs, & depots.
+addappid(3768760) -- 007 First Light
+addappid(3768761, 1, "8284c5e8921ec5acbe53f4d45af0260905841dc698ca179dd218ce5a2657ef09") -- Depot 3768761
+setManifestid(3768761, "7322178669849310269", 57179326292)
+addappid(3950810) -- 007 First Light - Gleaming Earphones (Gadget Skin)
+addappid(3950820) -- 007 First Light - Gleaming Lighter (Gadget Skin)
+addappid(3950830) -- 007 First Light - Gleaming Pen (Gadget Skin)
+addappid(3950850) -- 007 First Light - Gleaming Phone (Gadget Skin)
+addappid(3950870) -- 007 First Light - Day of the Dead (Exclusive Outfit)
+addappid(3950880) -- 007 First Light - Desert Explorer (Exclusive Outfit)
+addappid(3950900) -- 007 First Light - Gentleman Operator (Exclusive Outfit)
+addappid(3950910) -- 007 First Light - Silent Anchor (Exclusive Outfit) 
+addappid(3950930) -- 007 First Light - Agents Mark (Weapon Skin)
+addappid(3954230) -- 007 First Light - Golden Gun (Weapon Skin)
+addappid(3954250) -- 007 First Light - Obsidian Suit (Exclusive Outfit)
+addappid(3954260) -- 007 First Light - Classic Tuxedo (Exclusive Outfit) 
+addappid(4521800) -- 007 First Light - First Class (Outfit)
+addappid(4521810) -- 007 First Light - Forest Operative (Exclusive Outfit)
+addappid(4521820) -- 007 First Light - Observant Spy (Outfit)
+addappid(4521830) -- 007 First Light - Leisure Beam (Outfit)
+addappid(4521840) -- 007 First Light - One More (Outfit)
+addappid(4601250) -- 007 First Light - Diamond in the Rough (Outfit)
+addappid(4626160) -- 007 First Light - Standard
+addappid(4707780) -- 007 First Light - 00 Pack
